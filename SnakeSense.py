440fc49925cad99f70f8d6e557fc2c4e1e6edb6e
mrstockis <mrstockis@github.com>
2018-03-30 23:42:20 +0000

    minor fixes

--- SnakeSense.py
+++ SnakeSense.py
@@ -1,16 +1,18 @@
 
 from time import sleep
 from sense_hat import SenseHat
-from random import randint
+from random import randint,choice
 
 sense = SenseHat()
 
-Li =[100,0,100]
 R = [100,0,0]
 G = [0,100,0]
 B = [0,0,100]
 blank = [0,0,0]
+Cy =[0,70,90]
+Li =[100,0,100]
 
+Mix = [R,Cy,Li]
 
 def Paint(things):
     sheet = [ blank for i in range(64) ]
@@ -39,14 +41,13 @@ def gameLoop():
     score = 0
     x,y = 4,3
     angle = "right"
-    snake = [         [3,3,B] , [x,y,B] ]   # [2,3,B] 
-    apple = [[randint(0,7),randint(0,7),Li]]
+    snake = [ [2,3,G], [3,3,G] , [x,y,G] ]
+    apple = [[randint(0,7),randint(0,7),choice(Mix)]]
 
     try:
         while True:
             
             event = sense.stick.get_events()
- #           print(event)
             if len(event):
                 if event[0][1] != "middle":
                     angle = event[0][1]
@@ -59,23 +60,22 @@ def gameLoop():
                         sense.show_message("Bye!",text_colour=B)
                         sleep(2)
                         sense.clear(); quit()
-                    sense.show_message("Score: {}".format(score),text_colour=B)
+                    sense.show_message("Score  {}".format(score),text_colour=B)
                     sleep(4)
                     gameLoop()
             
-            snake.append([x,y,B]) 
+            snake.append([x,y,G]) 
             
             if x == apple[0][0] and y == apple[0][1]:
-                apple = [[randint(0,7),randint(0,7),Li]]
+                apple = [[randint(0,7),randint(0,7),choice(Mix)]]
                 score += 1
             else: snake.pop(0)
             
             sense.set_pixels(Paint([apple,snake]))
-#            print("VectorValue:",vect)
- #           print("SnakeList:",snake)
-  #          print("ApplePos:",apple)
             sleep(0.3)
+
     except: KeyboardInterrupt
+
     sense.clear()
     quit()
 
